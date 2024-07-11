50866bb5fda5db9f06181ddcc968b9a83cf1b581
seveibar <seveibar@gmail.com>
2024-07-11 14:30:10 -0700

    add applySelector to soup-util

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * from "./lib/su"
 export * from "./lib/transform-soup-elements"
 export * from "./lib/direction-to-vec"
+export * from "./lib/apply-selector"
 
 export { default as su } from "./lib/su"
---
+++ lib/convert-abbreviation-to-soup-element-type.ts
@@ -0,0 +1,11 @@
+export const convertAbbrToType = (abbr: string): string => {
+  switch (abbr) {
+    case "port":
+      return "source_port"
+    case "net":
+      return "source_net"
+    case "power":
+      return "simple_power_source"
+  }
+  return abbr
+}
