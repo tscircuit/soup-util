27c941d2bc0cbcb0d1ffd2e1520b315e5e306b0d
seveibar <seveibar@gmail.com>
2024-09-20 12:19:57 -0700

    add get bounds of pcb elements

--- index.ts
+++ index.ts
@@ -5,5 +5,6 @@ export * from "./lib/apply-selector"
 export * from "./lib/get-element-by-id"
 export * from "./lib/get-element-id"
 export * from "./lib/readable-name-functions/get-readable-name-for-element"
+export * from "./lib/get-bounds-of-pcb-elements"
 
 export { default as su } from "./lib/su"
